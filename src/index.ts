bcbfa0cc3e2550d044ef0ed458de0832ca1c49f7
donvincenzo <github@donvincenzo.com>
2021-05-10 20:31:25 +0400

    Searching through all block's Tx instead of lasts

--- src/index.ts
+++ src/index.ts
@@ -272,8 +272,12 @@ export class FlashbotsBundleProvider extends providers.JsonRpcProvider {
         } else {
           const block = await this.genericProvider.getBlock(targetBlockNumber)
           // check bundle against block:
+          const blockTransactionsHash: {[key:string]: boolean} = {};
+          for(const bt of block.transactions){
+            blockTransactionsHash[bt] = true;
+          }
           const bundleIncluded = transactionAccountNonces.every(
-            (transaction, i) => block.transactions[block.transactions.length - 1 - i] === transaction.hash
+            (transaction) => blockTransactionsHash[transaction.hash] === true
           )
           resolve(bundleIncluded ? FlashbotsBundleResolution.BundleIncluded : FlashbotsBundleResolution.BlockPassedWithoutInclusion)
         }
